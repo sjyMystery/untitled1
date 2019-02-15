405981a5aea079103b4f484b085bf707a3873ca4
nathaniel <348831271@qq.com>
2019-02-16 06:52:35 +0800

    ...

--- a3c_test.py
+++ a3c_test.py
@@ -3,12 +3,19 @@ from A3C import Master
 from TradeEnv import Env
 import datetime
 import numpy as np
+import pandas as pd
 
 tf.flags.DEFINE_integer('workers', 4, """co-workers num""", 1)
 tf.flags.DEFINE_string('instrument', "USDJPY", """name of instruments""")
 tf.flags.DEFINE_integer('per_bin', 1000, """log state per bin""", 1000)
 tf.flags.DEFINE_integer('per_trade', 100, """log state per trade""", 100)
 tf.flags.DEFINE_integer('time_length', 60 * 24 * 30, """time step length""", 1)
+tf.flags.DEFINE_integer('eps', 100, """""")
+tf.flags.DEFINE_string('start_date', '2012-01-01', """train begin date""")
+tf.flags.DEFINE_string('end_date', '2013-01-01', """train end date""")
+
+start_date = pd.to_datetime(tf.flags.FLAGS.start_date)
+end_date = pd.to_datetime(tf.flags.FLAGS.end_date)
 
 
 def action_convert(action):
@@ -20,8 +27,8 @@ def action_convert(action):
 env = Env(tf.flags.FLAGS.instrument, 100000, action_convert=action_convert,
           log_per_bin=tf.flags.FLAGS.per_bin,
           log_per_trade=tf.flags.FLAGS.per_trade,
-          start_date=datetime.datetime(2012, 1, 1, 0, 0, 0),
-          end_date=datetime.datetime(2013, 1, 1, 0, 0, 0))
+          start_date=start_date,
+          end_date=end_date)
 a3c = Master(env.clone, n_workers=tf.flags.FLAGS.workers, time_length=tf.flags.FLAGS.time_length)
 
 a3c.run()
