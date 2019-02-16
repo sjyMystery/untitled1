94258eaa16add295c193e6b3fb572c8ce8e18312
nathaniel <348831271@qq.com>
2019-02-16 09:22:44 +0800

    ....

--- A3C/Master.py
+++ A3C/Master.py
@@ -4,11 +4,6 @@ from A3C.Worker import Worker
 from A3C.str import GLOBAL_NET_SCOPE
 
 import threading
-import multiprocessing
-
-
-def _work(worker):
-    worker.work()
 
 
 class Master:
@@ -25,21 +20,22 @@ class Master:
         n_action = env.action_space.shape[0]
         a_range = [env.action_space.low, env.action_space.high]
 
-        self.__opt_a = tf.train.RMSPropOptimizer(lr_actor, name='RMSPropA')
-        self.__opt_c = tf.train.RMSPropOptimizer(lr_critic, name='RMSPropC')
-        self.__global_ac = Net(GLOBAL_NET_SCOPE, n_state=n_state, n_action=n_action, a_range=a_range,
-                               sess=self.__sess, op_actor=self.__opt_a, time_length=time_length,
-                               op_critic=self.__opt_c, beta=beta)  # we only need its params
-        self.__workers = []
-        # Create worker
-        for i in range(n_workers):
-            i_name = 'A3C_Worker_%i' % i  # worker name
-            self.__workers.append(
-                Worker(master=self, name=i_name, make_env=make_env, gamma=gamma,
-                       op_actor=self.__opt_a,
-                       op_critic=self.__opt_c, beta=beta, update_global_iter=update_global_iter,
-                       time_length=time_length,
-                       global_ac=self.__global_ac))
+        with 1 as a:
+            self.__opt_a = tf.train.RMSPropOptimizer(lr_actor, name='RMSPropA')
+            self.__opt_c = tf.train.RMSPropOptimizer(lr_critic, name='RMSPropC')
+            self.__global_ac = Net(GLOBAL_NET_SCOPE, n_state=n_state, n_action=n_action, a_range=a_range,
+                                   sess=self.__sess, op_actor=self.__opt_a, time_length=time_length,
+                                   op_critic=self.__opt_c, beta=beta)  # we only need its params
+            self.__workers = []
+            # Create worker
+            for i in range(n_workers):
+                i_name = 'A3C_Worker_%i' % i  # worker name
+                self.__workers.append(
+                    Worker(master=self, name=i_name, make_env=make_env, gamma=gamma,
+                           op_actor=self.__opt_a,
+                           op_critic=self.__opt_c, beta=beta, update_global_iter=update_global_iter,
+                           time_length=time_length,
+                           global_ac=self.__global_ac))
 
         self.__coord = tf.train.Coordinator()
         self.__sess.run(tf.global_variables_initializer())
@@ -54,9 +50,12 @@ class Master:
         return self.__sess
 
     def run(self):
-        with multiprocessing.Pool(len(self.__workers)) as pool:
-            result = pool.map(_work, self.__workers)
-            print(result)
+        for worker in self.__workers:
+            t = threading.Thread(target=(lambda: worker.work()))
+            t.start()
+            self.__worker_threads.append(t)
+
+        self.__coord.join(self.__worker_threads)
 
     @property
     def coord(self):
